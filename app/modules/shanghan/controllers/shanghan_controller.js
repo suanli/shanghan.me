090d784cb49ff2ddf9ffbf8baab757abeb1d2da1
Ryan Zhao <suanli@gmail.com>
2014-06-12 13:46:06 +0800

    Use promise for async data process. But seems the output data need a better way out.

--- app/modules/shanghan/controllers/shanghan_controller.js
+++ app/modules/shanghan/controllers/shanghan_controller.js
@@ -23,9 +23,15 @@ define(['angular', '../module'], function (ng) {
 		});
 
 	ng.module('shanghan.controllers')
-		.factory('httpGet',
+		.factory('dshQuery',
 			function($http, $q)
 			{
+				var chapter;
+				var items = {};
+				var herb;
+				var weight;
+				var recipes = {};
+
 				var get = function(url) {
 					var deferred = $q.defer();
 					$http.get(url).success(function(data) {
@@ -34,50 +40,140 @@ define(['angular', '../module'], function (ng) {
 					// TODO: Error case?
 					return deferred.promise;
 				};
-				return {
-					get: get
+
+				var getTitle = function (index){
+					if(chapter && chapter[index] == index)
+					{
+						var q = $q.defer();
+						q.resolve();
+						return q.promise;
+					}
+					return get('modules/shanghan/data/shlglb/ch'+index+'/title.json')
+						.then(function (data){
+							chapter = data;
+						});
 				};
 
-			});
+				var getItem = function (chapter, index){
+					if(items[index] && items[index].chapter == chapter)
+					{
+						var q = $q.defer();
+						q.resolve();
+						return q.promise;
+					}
+					return get('modules/shanghan/data/shlglb/ch'+chapter+'/'+index+'.json')
+						.then(function (data){
+							items[index] = data;
+						});
+				};
+
+				var getHerb = function (){
+					if(herb)
+					{
+						var q = $q.defer();
+						q.resolve();
+						return q.promise;
+					}
+					return get('modules/shanghan/data/shlglb/herb/herb.json')
+						.then(function (data){
+							herb = data;
+						});
+				};
+
+				var getWeight = function (){
+					if(weight)
+					{
+						var q = $q.defer();
+						q.resolve();
+						return q.promise;
+					}
+					return get('modules/shanghan/data/shlglb/herb/weight.json')
+						.then(function (data){
+							weight = data;
+						});
+				};
+
+				var getRecipe = function (name){
+					if(recipes[name])
+					{
+						var q = $q.defer();
+						q.resolve();
+						return q.promise;
+					}
+					return get('modules/shanghan/data/shlglb/recipe/' + name + '.json')
+						.then(function (data){
+							recipes[name] = data;
+						});
+				};
+
+				var formatRecipe = function (){
+					for(var i in items)
+					{
+						var item = items[i];
+						if(item.recipe){
+							if(recipes[item.recipe]){
+								item.recipe = recipes[item.recipe];
+								for(var j in item.recipe.items)
+								{
+									var h = item.recipe.items[j];
+									if(herb[h.herb]){
+										h.herb = herb[h.herb].name;
+									} else{
+										// Error case, not this herb.
+									}
+									if(weight[h.weight]){
+										h.weight = weight[h.weight].handai;
+									} else{
+										// Error case, not this herb.
+									}
+								}
+							} else{
+									//Error case, not get recipe yet.
+							}
+						}
+					}
+				};
+
+				return {
+					getTitle: getTitle,
+					getHerb: getHerb,
+					getWeight: getWeight,
+					getItem: getItem,
+					getRecipe: getRecipe,
+					formatRecipe: formatRecipe,
+					items: items,
+					recipes: recipes,
+					chapter: chapter
+				};
+			}
+		);
 
 	ng.module('shanghan.controllers')
 		.controller('mainItemController', [
 			'$scope',
-			'httpGet',
-			function ($scope, httpGet) {
-				//TODO: Refactor for better writing logic.
-				$scope.items = new Array();
+			'$q',
+			'dshQuery',
+			function ($scope, $q, dshQuery) {
 
-				$scope.chapter = httpGet.get('modules/shanghan/data/shlglb/ch06/title.json')
-					.then(function(data) {
-					$scope.chapter = data;
-				});
-
-				var itemText = null;
-				httpGet.get('modules/shanghan/data/shlglb/ch06/13.json')
-					.then(function (data) {
-						itemText = data;
-						return httpGet.get('modules/shanghan/data/shlglb/recipe/' + itemText.recipe + '.json');
-					}).then(function (recipe) {
-						itemText.recipe = recipe;
-						return httpGet.get('modules/shanghan/data/shlglb/herb/herb.json');
-					}).then(function (herb) {
-						itemText.herb = herb;
-						return httpGet.get('modules/shanghan/data/shlglb/herb/weight.json');
-					}).then(function (weight) {
-						itemText.weight = weight;
-						for(var a = 0; a < itemText.recipe.items.length; a++)
-						{
-							itemText.recipe.items[a].herb = itemText.herb[itemText.recipe.items[a].herb].name;
-							itemText.recipe.items[a].weight = itemText.weight[itemText.recipe.items[a].weight].handai;
-						}
-						$scope.items.push(itemText);
+				$q.all([
+						dshQuery.getTitle(6),
+						dshQuery.getItem(6, 12),
+						dshQuery.getItem(6, 13),
+						dshQuery.getHerb(),
+						dshQuery.getWeight()
+					])
+					.then(function (){
+							$q.all([
+									dshQuery.getRecipe(dshQuery.items[13].recipe)
+								])
+								.then(function (){
+									// Format recipe.
+									dshQuery.formatRecipe();
+									$scope.chapter = dshQuery.chapter;
+									$scope.items = dshQuery.items;
+									console.log(dshQuery);
+								})
 					});
-
-				httpGet.get('modules/shanghan/data/shlglb/ch06/12.json')
-					.then(function(data) {
-					$scope.items.push(data);
-				});
 			}
 		]
 	);
